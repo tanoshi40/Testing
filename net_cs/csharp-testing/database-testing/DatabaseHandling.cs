d3a6f6b884514dfcabc53b36ef7762ecde703be4
tanoshi <tanoshi40@gmail.com>
2024-03-15 15:10:06 +0100

    Add database interaction and project setup
    Added new classes for database interaction, set up SQL dialect mappings, and included a mocking project reference alongside necessary MySQL connectors. Integrated these elements into the solution to facilitate database testing within the C# testing framework.

---
+++ net_cs/csharp-testing/database-testing/DatabaseHandling.cs
@@ -0,0 +1,45 @@
+﻿using mocking;
+using MySqlConnector;
+
+namespace database_testing;
+
+/*
+ create table person
+(
+    Id         char(36)     not null
+        primary key,
+    FirstName  varchar(255) not null,
+    LastName   varchar(255) not null,
+    Street     varchar(255) not null,
+    City       varchar(255) not null,
+    State      varchar(255) not null,
+    PostalCode varchar(255) not null
+);
+*/
+
+public static class DatabaseHandling
+{
+    private const string DbConnectionString = "server=127.0.0.1;uid=root;database=testing";
+
+    public static async Task InsertPersonAsync(Person person, MySqlConnection connection)
+    {
+        // SQL INSERT statement.
+        const string sql = """
+                            INSERT INTO Person (Id, FirstName, LastName, Street, City, State, PostalCode)
+                            VALUES (@Id, @FirstName, @LastName, @Street, @City, @State, @PostalCode)
+                           """;
+        using var command = new MySqlCommand(sql, connection);
+
+        // Setting the parameters.
+        command.Parameters.AddWithValue("@Id", person.Id.ToString());
+        command.Parameters.AddWithValue("@FirstName", person.FirstName);
+        command.Parameters.AddWithValue("@LastName", person.LastName);
+        command.Parameters.AddWithValue("@Street", person.Street);
+        command.Parameters.AddWithValue("@City", person.City);
+        command.Parameters.AddWithValue("@State", person.State);
+        command.Parameters.AddWithValue("@PostalCode", person.PostalCode);
+
+        // Execute the INSERT statement.
+        await command.ExecuteNonQueryAsync();
+    }
+}
--- net_cs/csharp-testing/database-testing/Program.cs
+++ net_cs/csharp-testing/database-testing/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Test :)");
+﻿using database_testing;
+
+Console.WriteLine("Test :)");
+
