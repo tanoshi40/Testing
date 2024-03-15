82d11ef1a2c12292bf99872da4fd14b2aacf127c
tanoshi <tanoshi40@gmail.com>
2024-03-15 16:58:28 +0100

    Refactor and enhance database operations
    Refactor DatabaseHandling.cs to improve insert operations and code structure. Add various insert methods, including transactional and bulk insertions. Update connection string to enable local file loading and include additional using directives for required namespaces.

--- net_cs/csharp-testing/database-testing/DatabaseHandling.cs
+++ net_cs/csharp-testing/database-testing/DatabaseHandling.cs
@@ -1,4 +1,6 @@
-﻿using mocking;
+﻿using System.Data;
+using System.Text;
+using mocking;
 using MySqlConnector;
 
 namespace database_testing;
@@ -19,18 +21,27 @@ namespace database_testing;
 
 public static class DatabaseHandling
 {
-    private const string DbConnectionString = "server=127.0.0.1;uid=root;database=testing";
+    private const string DbConnectionString = "server=127.0.0.1;uid=root;database=testing;AllowLoadLocalInfile=True";
 
-    public static async Task InsertPersonAsync(Person person, MySqlConnection connection)
+    private const string InsertPersonSql = """
+                                           INSERT INTO person (Id, FirstName, LastName, Street, City, State, PostalCode)
+                                           VALUES (@Id, @FirstName, @LastName, @Street, @City, @State, @PostalCode)
+                                           """;
+
+    public static MySqlConnection GetConnection() => new(DbConnectionString);
+
+    public static async Task Truncate(MySqlConnection connection)
     {
-        // SQL INSERT statement.
-        const string sql = """
-                            INSERT INTO Person (Id, FirstName, LastName, Street, City, State, PostalCode)
-                            VALUES (@Id, @FirstName, @LastName, @Street, @City, @State, @PostalCode)
-                           """;
-        using var command = new MySqlCommand(sql, connection);
+        const string sql = "truncate table person";
 
-        // Setting the parameters.
+        await using var command = new MySqlCommand(sql, connection);
+        await command.ExecuteNonQueryAsync();
+    }
+
+    public static async Task InsertPersonAsync(Person person, MySqlConnection connection,
+        MySqlTransaction? transaction = null)
+    {
+        await using var command = new MySqlCommand(InsertPersonSql, connection, transaction);
         command.Parameters.AddWithValue("@Id", person.Id.ToString());
         command.Parameters.AddWithValue("@FirstName", person.FirstName);
         command.Parameters.AddWithValue("@LastName", person.LastName);
@@ -39,7 +50,170 @@ public static class DatabaseHandling
         command.Parameters.AddWithValue("@State", person.State);
         command.Parameters.AddWithValue("@PostalCode", person.PostalCode);
 
-        // Execute the INSERT statement.
         await command.ExecuteNonQueryAsync();
+    }
+
+    public static async Task InsertPersonsAsyncPrimitive(IEnumerable<Person> persons, MySqlConnection connection)
+    {
+        foreach (var person in persons)
+        {
+            await InsertPersonAsync(person, connection);
+        }
+    }
+
+    public static async Task InsertPersonsAsyncTransaction(IEnumerable<Person> persons, MySqlConnection connection)
+    {
+        await using var transaction = await connection.BeginTransactionAsync();
+        foreach (var person in persons)
+        {
+            await InsertPersonAsync(person, connection, transaction);
+        }
+
+        await transaction.CommitAsync();
+    }
+
+    public static async Task InsertPersonsAsyncBulkStringBuilder(IEnumerable<Person> persons,
+        MySqlConnection connection, int batchSize = 5_000)
+    {
+        const string initialSql =
+            "INSERT INTO Person (Id, FirstName, LastName, Street, City, State, PostalCode) VALUES ";
+        var sql = new StringBuilder(initialSql);
+
+        var parameters = new List<MySqlParameter>();
+        var values = new List<string>();
+
+        var count = 0;
+        foreach (var person in persons)
+        {
+            var personValues =
+                $"(@Id{count}, @FirstName{count}, @LastName{count}, @Street{count}, @City{count}, @State{count}, @PostalCode{count})";
+            values.Add(personValues);
+
+            parameters.AddRange(new[]
+            {
+                new MySqlParameter($"@Id{count}", person.Id.ToString()),
+                new MySqlParameter($"@FirstName{count}", person.FirstName),
+                new MySqlParameter($"@LastName{count}", person.LastName),
+                new MySqlParameter($"@Street{count}", person.Street),
+                new MySqlParameter($"@City{count}", person.City),
+                new MySqlParameter($"@State{count}", person.State),
+                new MySqlParameter($"@PostalCode{count}", person.PostalCode),
+            });
+            count++;
+
+            if (count < batchSize)
+            {
+                continue;
+            }
+
+            await SendBulkInsertCandles(connection, sql, values, parameters);
+            parameters.Clear();
+            values.Clear();
+            sql.Clear();
+            sql.Append(initialSql);
+            count = 0;
+        }
+
+        if (count > 0)
+        {
+            await SendBulkInsertCandles(connection, sql, values, parameters);
+        }
+    }
+
+    private static async Task SendBulkInsertCandles(MySqlConnection connection, StringBuilder sql,
+        IEnumerable<string> values, List<MySqlParameter> parameters)
+    {
+        sql.Append(string.Join(",", values));
+        sql.Append(';');
+
+        await using var command = new MySqlCommand(sql.ToString(), connection);
+        command.Parameters.AddRange(parameters.ToArray());
+
+        await command.ExecuteNonQueryAsync();
+    }
+
+    public static async Task InsertPersonAsyncBulkDataTable(IEnumerable<Person> persons, MySqlConnection connection)
+    {
+        const string srcTable = "person";
+
+        var adapterSelect = new MySqlDataAdapter($"SELECT * FROM {srcTable} LIMIT 0", connection);
+
+        var ds = new DataSet();
+        adapterSelect.Fill(ds, srcTable);
+
+        MySqlTransaction transaction = await connection.BeginTransactionAsync();
+
+        var dataAdapter = new MySqlDataAdapter();
+        dataAdapter.InsertCommand = new MySqlCommand(InsertPersonSql, connection);
+
+        dataAdapter.InsertCommand.Transaction = transaction;
+
+        dataAdapter.InsertCommand!.Parameters.Add(new MySqlParameter("@Id", MySqlDbType.VarChar, 36, "Id"));
+        dataAdapter.InsertCommand.Parameters.Add(
+            new MySqlParameter("@FirstName", MySqlDbType.VarChar, 255, "FirstName"));
+        dataAdapter.InsertCommand.Parameters.Add(new MySqlParameter("@LastName", MySqlDbType.VarChar, 255, "LastName"));
+        dataAdapter.InsertCommand.Parameters.Add(new MySqlParameter("@Street", MySqlDbType.VarChar, 255, "Street"));
+        dataAdapter.InsertCommand.Parameters.Add(new MySqlParameter("@City", MySqlDbType.VarChar, 255, "City"));
+        dataAdapter.InsertCommand.Parameters.Add(new MySqlParameter("@State", MySqlDbType.VarChar, 255, "State"));
+        dataAdapter.InsertCommand.Parameters.Add(new MySqlParameter("@PostalCode", MySqlDbType.VarChar, 255,
+            "PostalCode"));
+
+        await dataAdapter.InsertCommand.PrepareAsync();
+
+        dataAdapter.InsertCommand.UpdatedRowSource = UpdateRowSource.None;
+
+        foreach (var person in persons)
+        {
+            var dataTable = ds.Tables[srcTable]!;
+            var row = dataTable.NewRow();
+
+            row["Id"] = person.Id.ToString();
+            row["FirstName"] = person.FirstName;
+            row["LastName"] = person.LastName;
+            row["Street"] = person.Street;
+            row["City"] = person.City;
+            row["State"] = person.State;
+            row["PostalCode"] = person.PostalCode;
+            dataTable.Rows.Add(row);
+        }
+
+        dataAdapter.UpdateBatchSize = 5_000;
+        dataAdapter.Update(ds, srcTable);
+        await transaction.CommitAsync();
+    }
+
+    public static async Task InsertPersonAsyncBulkCopy(IEnumerable<Person> persons, MySqlConnection connection)
+    {
+        // Create a DataTable to hold the data
+        var dataTable = new DataTable();
+        dataTable.Columns.Add("Id");
+        dataTable.Columns.Add("FirstName");
+        dataTable.Columns.Add("LastName");
+        dataTable.Columns.Add("Street");
+        dataTable.Columns.Add("City");
+        dataTable.Columns.Add("State");
+        dataTable.Columns.Add("PostalCode");
+
+        // Add the person data to the DataTable
+        foreach (var person in persons)
+        {
+            dataTable.Rows.Add(
+                person.Id.ToString(),
+                person.FirstName,
+                person.LastName,
+                person.Street,
+                person.City,
+                person.State,
+                person.PostalCode
+            );
+        }
+
+        // Perform the bulk insert
+        var bulkCopy = new MySqlBulkCopy(connection)
+        {
+            DestinationTableName = "person"
+        };
+
+        await bulkCopy.WriteToServerAsync(dataTable);
     }
 }
--- net_cs/csharp-testing/database-testing/Program.cs
+++ net_cs/csharp-testing/database-testing/Program.cs
@@ -1,4 +1,150 @@
-﻿using database_testing;
+﻿using System.Diagnostics;
+using database_testing;
+using mocking;
+using MySqlConnector;
 
-Console.WriteLine("Test :)");
+Console.WriteLine("Connection");
 
+// await Warmup();
+
+
+await InsertDataTable();
+await InsertStringBuilder();
+await InsertBulkCopy();
+// await InsertTransaction();
+// await InsertPrimitive();
+
+Console.WriteLine("Done");
+return;
+
+async Task InsertPrimitive()
+{
+    Console.WriteLine("Primitive");
+    var persons = CreatePersons();
+    var stopwatch = new Stopwatch();
+
+    await using var mySqlConnection = CreateConnection();
+    await mySqlConnection.OpenAsync();
+
+    stopwatch.Start();
+    await DatabaseHandling.InsertPersonsAsyncPrimitive(persons, mySqlConnection);
+    stopwatch.Stop();
+
+    await DatabaseHandling.Truncate(mySqlConnection);
+
+    var time = stopwatch.Elapsed;
+    Console.WriteLine(time);
+}
+
+async Task InsertTransaction()
+{
+    Console.WriteLine("Transaction");
+    var persons = CreatePersons();
+    var stopwatch = new Stopwatch();
+
+    await using var mySqlConnection = CreateConnection();
+    await mySqlConnection.OpenAsync();
+
+    stopwatch.Start();
+    await DatabaseHandling.InsertPersonsAsyncTransaction(persons, mySqlConnection);
+    stopwatch.Stop();
+
+    await DatabaseHandling.Truncate(mySqlConnection);
+
+    var time = stopwatch.Elapsed;
+    Console.WriteLine(time);
+}
+
+async Task InsertStringBuilder()
+{
+    Console.WriteLine("StringBuilder");
+    var persons = CreatePersons();
+    var stopwatch = new Stopwatch();
+
+    await using var mySqlConnection = CreateConnection();
+    await mySqlConnection.OpenAsync();
+
+    stopwatch.Start();
+    await DatabaseHandling.InsertPersonsAsyncBulkStringBuilder(persons, mySqlConnection);
+    stopwatch.Stop();
+
+    await DatabaseHandling.Truncate(mySqlConnection);
+
+    var time = stopwatch.Elapsed;
+    Console.WriteLine(time);
+}
+
+async Task InsertBulkCopy()
+{
+    Console.WriteLine("BulkCopy");
+    var persons = CreatePersons();
+    var stopwatch = new Stopwatch();
+
+    await using var mySqlConnection = CreateConnection();
+    await mySqlConnection.OpenAsync();
+
+    stopwatch.Start();
+    await DatabaseHandling.InsertPersonAsyncBulkCopy(persons, mySqlConnection);
+    stopwatch.Stop();
+
+    await DatabaseHandling.Truncate(mySqlConnection);
+
+    var time = stopwatch.Elapsed;
+    Console.WriteLine(time);
+}
+
+async Task InsertDataTable()
+{
+    Console.WriteLine("DataSet");
+    var persons = CreatePersons();
+    var stopwatch = new Stopwatch();
+
+    await using var mySqlConnection = CreateConnection();
+    await mySqlConnection.OpenAsync();
+
+    stopwatch.Start();
+    await DatabaseHandling.InsertPersonAsyncBulkDataTable(persons, mySqlConnection);
+    stopwatch.Stop();
+
+    await DatabaseHandling.Truncate(mySqlConnection);
+
+    var time = stopwatch.Elapsed;
+    Console.WriteLine(time);
+}
+
+IEnumerable<Person> CreatePersons()
+{
+    // Console.WriteLine("Creating Data");
+    return Person.GeneratePersons(50_000).ToArray();
+}
+
+MySqlConnection CreateConnection()
+{
+    // Console.WriteLine("Creating Connection");
+    return DatabaseHandling.GetConnection();
+}
+
+async Task Warmup()
+{
+    Console.WriteLine("WARMUP");
+    Console.WriteLine("WARMUP");
+    Console.WriteLine("WARMUP");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+
+    for (var i = 0; i < 10; i++)
+    {
+        await InsertDataTable();
+    }
+
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("WARMUP DONE");
+    Console.WriteLine("WARMUP DONE");
+    Console.WriteLine("WARMUP DONE");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+}
